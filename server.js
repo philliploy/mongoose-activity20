077cbfa92c53cdb66cf4128a3a8efed424d3dd86
philliploy <ploy3_98@yahoo.com>
2019-08-27 20:36:49 -0500

    live

--- server.js
+++ server.js
@@ -26,8 +26,13 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// // Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
 
